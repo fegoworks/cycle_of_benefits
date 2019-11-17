f666cb9ea8f97a52d977a937204ecbc18c049a28
colenocks <enockscoleman@gmail.com>
2019-11-17 23:38:10 +0000

    created a div to display alert info for form validation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// "use strict";
 const menuBtn = document.querySelector(".menu-btn");
 const menuHeader = document.querySelector(".menu-header");
 
@@ -19,4 +20,30 @@ function menuToggle() {
     //reset Menu State
     showMenu = false;
   }
+}
+
+//ALERT HANDLER
+// put the style contents in a style sheet and add just classes instead
+let alertInfo = "";
+alertInfo = "sup victoria";
+
+let alertDiv = document.createElement("div");
+alertDiv.classList.add("alert-info");
+alertDiv.append(document.createTextNode(alertInfo));
+
+// Alert Close button
+let closeButton = document.createElement("button");
+closeButton.classList.add("close-button");
+closeButton.append(document.createTextNode("close"));
+alertDiv.appendChild(closeButton);
+
+closeButton.onclick = closeAlert;
+
+// Add alert info div based on form events from login and signup pages
+function addAlert(msg, color) {
+  document.body.appendChild(alertDiv);
+}
+// close alert
+function closeAlert() {
+  alertDiv.style.display = "none";
 }
